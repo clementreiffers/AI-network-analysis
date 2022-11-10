3ba9427c3a9e3fb79688f04b120c2bc8dc7777b0
clementreiffers <reiffersclement@gmail.com>
2022-11-10 12:21:04 +0100

    fichiers avec formatage correct black

--- src/load_model.py
+++ src/load_model.py
@@ -7,18 +7,18 @@ from src.decisionTree import base
 
 
 def load_model(filename, X_test, Y_test):
-    loaded_model = pickle.load(open(filename, 'rb'))
+    loaded_model = pickle.load(open(filename, "rb"))
     result = loaded_model.score(X_test, Y_test)
     print(result)
 
-filename = 'finalized_model.sav'
+
+filename = "finalized_model.sav"
 
 
 y, X = base()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.2,
-                                                    random_state=200)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=200
+)
 
 load_model(filename, X_test, y_test)
-
