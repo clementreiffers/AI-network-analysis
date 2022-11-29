fd78475206b021963938bf1f6e3d22b821690435
clementreiffers <reiffersclement@gmail.com>
2022-11-29 10:03:04 +0100

    nettoyage test_model

--- src/test_model.py
+++ src/test_model.py
@@ -8,8 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from src.main_contants import TARGET
 
 
-def base():
-    df = pd.read_csv("../data/dataset_clean.csv")
+def get_x_y():
+    df = pd.read_csv("../data/Dataset-Unicauca-Version2-87Atts.csv")
     x_col = [
         "Source.Port",
         "Destination.Port",
@@ -32,32 +32,34 @@ def base():
         "act_data_pkt_fwd",
         "Fwd.Packet.Length.Max",
     ]
-    return df[TARGET], df[x_col]
+    return df[x_col], df[TARGET]
 
 
-y, X = base()
+if __name__ == "__main__":
+    x, y = get_x_y()
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=1
+    )
 
-"""
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-model = GaussianNB()
-"""
+    """
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    model = GaussianNB()
+    """
 
+    # model = SVC()
+    # model = SGDClassifier(loss="hinge", penalty="l2") # 0.4062
+    # model = MLPClassifier(hidden_layer_sizes=(30,), random_state=1, warm_start=True) # 0.5200
+    # model = LinearDiscriminantAnalysis()
+    # model = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
+    # model = make_pipeline(StandardScaler(), SGDClassifier(loss="hinge", penalty="l2"))
+    # model = make_pipeline(StandardScaler(), GaussianNB())
 
-# model = SVC()
-# model = SGDClassifier(loss="hinge", penalty="l2") # 0.4062
-# model = MLPClassifier(hidden_layer_sizes=(30,), random_state=1, warm_start=True) # 0.5200
-# model = LinearDiscriminantAnalysis()
-# model = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
-# model = make_pipeline(StandardScaler(), SGDClassifier(loss="hinge", penalty="l2"))
-# model = make_pipeline(StandardScaler(), GaussianNB())
+    model = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
 
-model = make_pipeline(StandardScaler(), LinearDiscriminantAnalysis())
-
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
-acc_dt = accuracy_score(y_test, y_pred)
-print("df test accuracy : {:.4f}".format(acc_dt))
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    acc_dt = accuracy_score(y_test, y_pred)
+    print("df test accuracy : {:.4f}".format(acc_dt))
